5f256db92b7a2b33cf5052e86dddafe51e2dbef2
udityaprakash <udityaprakash983@gmail.com>
2024-08-02 17:13:53 +0530

    added public folders and routes added

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/public',express.static("public"));
+app.use('/api/client/v1', require('./router/client'));
+app.use('/api/admin/v1', require('./router/admin'));
 
 app.get('/', (req, res) => {
     res.status(200).json({
         status: 'success',
         error: false,
         message: "Welcome to Anganwaadi API's"
+    });
+});
+
+app.all("*",(req,res)=>{
+    res.status(404).json({
+        status:'failure',
+        error:true,
+        message:"page not found/ api does'nt exist 😒."
     });
 });
 
