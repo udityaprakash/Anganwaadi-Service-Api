e948e3f59d921dfef8664bf45aba7afc394d02e5
udityaprakash <udityaprakash983@gmail.com>
2025-03-26 16:01:36 +0530

    added a message log schema

---
+++ src/database/schema/notificationHistory.schema.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const schema= new Schema({
+    message : {
+        type:String,
+        required:true
+    },
+    sendBy: {
+        type:Schema.Types.ObjectId,
+        ref: 'admin'
+    },
+    title: {
+        type:String,
+    },
+    sendTo: [
+        {
+            type:Schema.Types.ObjectId,
+            ref: 'client'
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // expires: 15552000 // 6 months in seconds is (6 * 30 * 24 * 60 * 60)
+        expires: 86400 //one day (1 * 24 * 60 * 60)
+    }
+},{timestamps:true});
+
+const result = mongoose.model("msgLog" , schema);
+
+module.exports = result;
